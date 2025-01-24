e51f16f5ebf082e832d10c3c22b8dd35545ff12e
unknown <claudia.vivo1213@gmail.com>
2025-01-24 12:16:57 +0100

    primersa subida de codigo

---
+++ JS/lista.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+  //Selectores
+  const createBtn = document.querySelector("#create");
+  const editarBtn = document.querySelector("#editar");
+
+  const contenedorTareas = document.querySelector("#contenedorTareas");
+
+  //Eventos
+  createBtn.addEventListener("click", cambiarPagina);
+  editarBtn.addEventListener("click", editarCliente);
+
+  //boton create task
+
+  function cambiarPagina() {
+    console.log("Cambiando");
+    window.location.href = "/HTML/toDoList.html";
+  }
+
+  //boton editar task
+
+  function editarCliente() {
+    window.location.href = "/HTML/toDoList.html";
+  }
+
+  console.log("hola");
+});
---
+++ JS/toDoList.js
@@ -0,0 +1,102 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let DB;
+
+  setTimeout(() => {
+    crearDB();
+  }, 1000);
+
+  //Selectores
+  const tituloInput = document.querySelector("#titulo");
+  const fechaInput = document.querySelector("#fecha");
+  const categoriaInput = document.querySelector("#categoria");
+  const descripcionInput = document.querySelector("#descripcion");
+
+  const contenedorTareas = document.querySelector("#contenedorTareas");
+
+  const formulario = document.querySelector("#formulario");
+  //Contenedor para las citas
+
+  const tareaObj = {
+    titulo: "",
+    fecha: "",
+    categoria: "",
+    descripcion: "",
+  };
+
+  //Eventos
+
+  tituloInput.addEventListener("change", datosTarea);
+  fechaInput.addEventListener("change", datosTarea);
+  categoriaInput.addEventListener("change", datosTarea);
+  descripcionInput.addEventListener("change", datosTarea);
+
+  formulario.addEventListener("submit", submitTarea);
+
+  //Clases
+  class AdminTareas {
+    constructor() {
+      this.tarea = [];
+    }
+    //metodo
+    agregar(tarea) {
+      this.tarea = [...this.tarea, tarea];
+    }
+    mostrar() {}
+  }
+
+  //Instanciamos una clase
+  const tareas = new AdminTareas();
+
+  //Funcion para llenar el objeto con los valores de las propiedades
+
+  function datosTarea(e) {
+    tareaObj[e.target.id] = e.target.value;
+  }
+
+  //Funcion para el boton de submit
+  function submitTarea(e) {
+    e.preventDefault();
+
+    tareas.agregar(tareaObj);
+    window.location.href = "/HTML/lista.html";
+  }
+
+  /// crear DB //////////////////////////////////////////
+
+  function crearDB() {
+    //Crear la base de datos
+
+    const crearDB = window.indexedDB.open("tareas", 1);
+
+    //Si hay un error
+    crearDB.onerror = function () {
+      console.log("Hubo un error");
+    };
+    //Si todo sale bien
+
+    crearDB.onsuccess = function () {
+      console.log("Todo bien");
+
+      DB = crearDB.result;
+
+      console.log(DB);
+    };
+
+    //Definir el schema
+
+    crearDB.onupgradeneeded = function (e) {
+      const db = e.target.result;
+
+      const objectStore = db.createObjectStore("tareas", {
+        keyPath: "id",
+        autoIncremet: "true",
+      });
+
+      //Definir todas las columnas
+      objectStore.createIndex("titulo", "titulo");
+      objectStore.createIndex("fecha", "fecha");
+      objectStore.createIndex("categoria", "categoria");
+      objectStore.createIndex("descripcion", "descripcion");
+    };
+  }
+});
